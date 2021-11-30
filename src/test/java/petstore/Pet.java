36ecfffdf3121e2050fe1f989de42800200682e9
Gabriel Henrique Wingerter Silva <gabriel.wingerter@hotmail.com>
2021-11-29 23:39:48 -0300

    Incluida assertivas para o body do teste deincluirPet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -11,6 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 //3- Classe
 public class Pet {
@@ -39,6 +40,8 @@ public class Pet {
         .then()
                 .log().all()
                 .statusCode(200)
+                .body("name",  is("Guilherme"))
+                .body("status", is("available"))
         ;
 
    }
